d8af400d4de549e50561679d387f3df9ab4db1e5
affter <after-shock@ya.ru>
2017-01-17 14:50:02 +0400

    Added Task 5

---
+++ TestTask/5. Finder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestTask
+{
+    class Finder
+    {
+        public static int FindUnique(int[] array)
+        {
+            HashSet<int> set = new HashSet<int>();
+            foreach (int i in array)
+            {
+                if (set.Contains(i))
+                    set.Remove(i);
+                else
+                    set.Add(i);
+            }
+            return set.Max();
+        }
+    }
+}
