ecbc6790012c65ba49546b6bad865686f6565fdb
affter <after-shock@ya.ru>
2017-01-17 11:44:40 +0400

    Added Task 1 and Task 2

---
+++ TestTask/1. Sort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestTask
+{
+    class Sort
+    {
+       public static void QSort(int[] array, int first, int last)
+        {
+            int temp;
+            int x = array[first + (last - first) / 2];
+            int i = first;
+            int j = last;
+            while (i <= j)
+            {
+                while (array[i] < x) i++;
+                while (array[j] > x) j--;
+                if (i <= j)
+                {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+            if (i < last)
+                QSort(array, i, last);
+            if (first < j)
+                QSort(array, first, j);
+        }
+    }
+}
+
---
+++ TestTask/2. ArrayProcessing.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestTask
+{
+    class ArrayProcessing
+    {
+        public static bool Contains(int[] array, int value)
+        {
+            int first = -1;
+            int last = array.Length;
+            int n;
+            if (value > array[last - 1] || value < array[0])
+                return false;
+            while (first < last - 1)
+            {
+                n = (first + last) / 2;
+                if (array[n] < value)
+                    first = n;
+                else
+                    last = n;
+            }
+            if (array[last] == value)
+                return true;
+            else
+                return false;
+        }
+        public static void Main()
+        {
+            int[] TestArray = { 3, 4, 65, 1, -5, -239, 33 };
+            Sort.QSort(TestArray, 0, TestArray.Length - 1);
+            Console.WriteLine(Contains(TestArray, 3));
+            Console.WriteLine(Contains(TestArray, 124));
+            Console.WriteLine(Contains(TestArray, 64));
+            Console.ReadKey();
+        }
+    }
+}
