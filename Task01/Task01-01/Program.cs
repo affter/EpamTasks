7ad2ef378e729e131984ebfdecd4d7a6950549f8
affter <after-shock@ya.ru>
2017-03-02 09:43:56 +0400

    Added Task01-01

--- Task01/Task01-01/Program.cs
+++ Task01/Task01-01/Program.cs
@@ -10,6 +10,24 @@ namespace Task01_01
     {
         static void Main(string[] args)
         {
+            int a, b;
+            do
+            {
+                Console.Write("Введите целое неотрицательное a: ");
+                a = int.Parse(Console.ReadLine());
+                if (a <= 0)
+                    Console.WriteLine("Некорректный ввод.");
+            }
+            while (a <= 0);
+            do
+            {
+                Console.Write("Введите целое неотрицательное b: ");
+                b = int.Parse(Console.ReadLine());
+                if (b <= 0)
+                    Console.WriteLine("Некорректный ввод.");
+            }
+            while (b <= 0);
+            Console.WriteLine($"Площадь прямоугольника S = {a * b}");
         }
     }
 }
