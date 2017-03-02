e5d8ca75f7b918c3d339c7b9984f43d3c61ec082
affter <after-shock@ya.ru>
2017-03-02 09:58:23 +0400

    Added Task01-02

---
+++ Task01/Task01-02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            do
+            {
+                Console.Write("Введите целое неотрицательное N: ");
+                if (!int.TryParse(Console.ReadLine(), out N) || N <= 0)
+                    Console.WriteLine("Некорректный ввод");
+            }
+            while (N <= 0);
+
+            for (int i = 1; i <= N; i++)
+                Console.WriteLine(new string('*', i));
+        }
+    }
+}
