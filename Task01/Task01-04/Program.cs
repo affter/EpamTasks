d75c2f1deeec08d1f78bcd2dd1d7f2fe03a1a9a7
affter <after-shock@ya.ru>
2017-03-02 11:09:32 +0400

    Update format in Task01-04

--- Task01/Task01-04/Program.cs
+++ Task01/Task01-04/Program.cs
@@ -18,13 +18,13 @@ namespace Task01_04
                     Console.WriteLine("Некорректный ввод");
             }
             while (N <= 0);
+            
             for (int i = 1; i <= N; i++)
                 for (int j = 1; j <= i; j++)
                 {
                     Console.Write(new string(' ', N - j));
                     Console.WriteLine(new string('*', 2 * j - 1));
                 }
-                    
         }
     }
 }
