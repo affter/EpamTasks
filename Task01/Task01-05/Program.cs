685e70d8b666cb045f46a5f1a66f7005538af3a4
affter <after-shock@ya.ru>
2017-03-02 10:27:59 +0400

    Added Task01-05

---
+++ Task01/Task01-05/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int a1 = 3,
+                            b1 = 5,
+                            n = 1000,
+                            c1 = a1 * b1,
+                            n1 = (n - 1) / a1,
+                            n2 = (n - 1) / b1,
+                            n3 = (n - 1) / c1,
+                            sum1 = a1 * (n1 + 1) * n1,
+                            sum2 = b1 * (n2 + 1) * n2,
+                            sum3 = c1 * (n3 + 1) * n3;
+            Console.WriteLine($"Сумма чисел, меньших тысячи, и кратных 3 или 5 равна {((sum1 + sum2 - sum3) / 2)}");
+        }
+    }
+}
