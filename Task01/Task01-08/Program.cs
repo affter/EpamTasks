7efad98fad7a4f23a5f754934d7a32a7805d309b
affter <after-shock@ya.ru>
2017-03-03 22:23:04 +0400

    Added Task01-08

---
+++ Task01/Task01-08/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Task01_08
+{
+    internal class Program
+    {
+        private static Random rand = new Random();
+
+        public static void Fill3DArray(int[,,] array)
+        {
+            for (int i = 0; i < array.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < array.GetUpperBound(1); j++)
+                {
+                    for (int k = 0; k < array.GetUpperBound(3); k++)
+                    {
+                        array[i, j, k] = rand.Next(-50, 50);
+                    }
+                }
+            }
+        }
+
+        public static void NullifyPositive(int[,,] array)
+        {
+            for (int i = 0; i < array.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < array.GetUpperBound(1); j++)
+                {
+                    for (int k = 0; k < array.GetUpperBound(3); k++)
+                    {
+                        if (array[i, j, k] > 0)
+                        {
+                            array[i, j, k] = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            int[,,] array = new int[10, 10, 10];
+            Fill3DArray(array);
+            NullifyPositive(array);
+        }
+    }
+}
