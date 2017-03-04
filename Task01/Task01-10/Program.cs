a5e92ce18b8500d02c08262345045b307848ad5e
affter <after-shock@ya.ru>
2017-03-04 10:14:39 +0400

    Added Task01-10

---
+++ Task01/Task01-10/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01_10
+{
+    internal class Program
+    {
+        private static Random rand = new Random();
+
+        private static void Fill2DArray(int[,] array)
+        {
+            for (int i = 0; i <= array.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= array.GetUpperBound(1); j++)
+                {
+                    array[i, j] = rand.Next(-50, 50);
+                }
+            }
+        }
+
+        private static void Write2DArray(int[,] array)
+        {
+            for (int i = 0; i <= array.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= array.GetUpperBound(1); j++)
+                {
+                    Console.Write($"{array[i, j]} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static int Sum(int[,] array)
+        {
+            int sum = 0;
+
+            for (int i = 0; i <= array.GetUpperBound(0); i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int j = 0; j <= array.GetUpperBound(0); j += 2)
+                    {
+                        sum += array[i, j];
+                    }
+                }
+                else
+                {
+                    for (int j = 1; j <= array.GetUpperBound(1); j += 2)
+                    {
+                        sum += array[i, j];
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        private static void Main(string[] args)
+        {
+            int[,] array = new int[5, 5];
+            Fill2DArray(array);
+            Write2DArray(array);
+            Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях равна {Sum(array)}");
+        }
+    }
+}
