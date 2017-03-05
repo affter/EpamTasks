9ed17331588864a6e07e7ff56a06234da5255b65
affter <after-shock@ya.ru>
2017-03-05 19:55:11 +0400

    Added Task01-11

---
+++ Task01/Task01-11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task01_11
+{
+    internal class Program
+    {
+        private static char[] AllSeparators()
+        {
+            var array = new char[char.MaxValue + 1];
+            int j = 0;
+
+            for (char i = (char)0; i < char.MaxValue; i++)
+            {
+                if (char.IsWhiteSpace(i) || char.IsSeparator(i) || char.IsPunctuation(i))
+                {
+                    array[j] = i;
+                    j++;
+                }
+            }
+
+            var output = new char[j];
+            Array.Copy(array, output, j);
+            return output;
+        }
+
+        private static void Main(string[] args)
+        {
+            var sum = 0.0;
+            Console.WriteLine("Введите строку:");
+            string input = Console.ReadLine();
+
+            string[] words = input.Split(AllSeparators());
+            var wordsLength = words.Length;
+            for (int i = 0; i < wordsLength; i++)
+            {
+                sum += words[i].Length;
+            }
+
+            Console.WriteLine($"Средняя длина слова во введенной строке: {sum / wordsLength}");
+        }
+    }
+}
