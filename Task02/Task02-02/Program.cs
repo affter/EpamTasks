6ec530e55735465743c9e97ccef84f43f660487d
affter <after-shock@ya.ru>
2017-03-10 14:08:41 +0400

    Added demonstration in Task02-02

--- Task02/Task02-02/Program.cs
+++ Task02/Task02-02/Program.cs
@@ -10,6 +10,18 @@ namespace Task02_02
     {
         private static void Main(string[] args)
         {
+            Triangle triangle = new Triangle(3, 4, 5);
+            Console.WriteLine($"Создан треугольник со сторонами {triangle.A},{triangle.B},{triangle.C}");
+            Console.WriteLine($"Периметр треугольника равен {triangle.Perimeter}");
+            Console.WriteLine($"Площадь треугольника равна {triangle.Area}");
+            triangle.A = 2;
+            Console.WriteLine($"Длина стороны a изменена. а = {triangle.A}");
+            triangle.B = 3;
+            Console.WriteLine($"Длина стороны b изменена. b = {triangle.B}");
+            triangle.C = 4;
+            Console.WriteLine($"Длина стороны c изменена. c = {triangle.C}");
+            Console.WriteLine($"Периметр треугольника равен {triangle.Perimeter}");
+            Console.WriteLine($"Площадь треугольника равна {triangle.Area}");
         }
     }
 }
