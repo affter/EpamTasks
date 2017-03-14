826c9706e375ac2b0c06a5468c845293efb039d3
affter <after-shock@ya.ru>
2017-03-14 12:58:19 +0400

    Added class Circle to FigureLibrary

---
+++ Task02/FigureLibrary/Circle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureLibrary
+{
+    public class Circle : RoundShape
+    {
+        public Circle(Point center, double radius) : base(center, radius)
+        {
+        }
+
+        public double Circumference { get => 2 * Math.PI * this.Radius; }
+    }
+}
