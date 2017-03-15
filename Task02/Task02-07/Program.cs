1b1251b903b2debddeecf349f9541ab658db4f9a
affter <after-shock@ya.ru>
2017-03-15 15:40:52 +0400

    Added Task02-07

--- Task02/Task02-07/Program.cs
+++ Task02/Task02-07/Program.cs
@@ -1,16 +1,206 @@
-﻿using FigureLibrary;
+﻿using System;
+using System.Collections.Generic;
+using FigureLibrary;
 
 namespace Task02_07
 {
     internal class Program
     {
+        private static void GetCircleInfo(out Point point, out double radius)
+        {
+            Console.Write("Введите координату x центра окружности: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Введите координату y центра окружности: ");
+            double y = double.Parse(Console.ReadLine());
+            point = new Point(x, y);
+            do
+            {
+                Console.Write("Введите радиус: ");
+                radius = double.Parse(Console.ReadLine());
+
+                if (radius <= 0)
+                {
+                    Console.WriteLine("Некорректный ввод: радиус должен быть больше нуля");
+                }
+            }
+            while (radius <= 0);
+        }
+
         private static void Main(string[] args)
         {
-            VectorGraphicsEditor.Draw(new Line(new Point(1, 1), new Point(2, 2)));
-            VectorGraphicsEditor.Draw(new Circle(new Point(1, 1), 2));
-            VectorGraphicsEditor.Draw(new Round(new Point(1, 1), 2));
-            VectorGraphicsEditor.Draw(new Ring(new Point(1, 1), 2, 3));
-            VectorGraphicsEditor.Draw(new Rectangle(new Point(1, 1), 2, 3));
+            var editor = new VectorGraphicsEditor();
+            ConsoleKeyInfo input;
+
+            while (true)
+            {
+                Console.WriteLine("Выберите режим работы:");
+                Console.WriteLine("Введите: ");
+                Console.WriteLine("\t 1: Создать объект");
+                Console.WriteLine("\t 2: Нарисовать все созданные объекты");
+                Console.WriteLine("\t Esc: Выход");
+
+                input = Console.ReadKey();
+                Console.WriteLine();
+                if (input.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+
+                switch (input.KeyChar)
+                {
+                    case '1':
+                        ConsoleKeyInfo insideKeyInfo;
+                        Console.WriteLine("Выберите, какую фигуру создать:");
+                        Console.WriteLine("Введите:");
+                        Console.WriteLine("\t 1: Линия");
+                        Console.WriteLine("\t 2: Окружность");
+                        Console.WriteLine("\t 3: Круг");
+                        Console.WriteLine("\t 4: Кольцо");
+                        Console.WriteLine("\t 5: Прямоугольник");
+                        Console.WriteLine("\t Esc: Назад");
+
+                        insideKeyInfo = Console.ReadKey();
+                        Console.WriteLine();
+                        if (input.Key == ConsoleKey.Escape)
+                        {
+                            break;
+                        }
+
+                        switch (insideKeyInfo.KeyChar)
+                        {
+                            case '1':
+                                try
+                                {
+                                    Console.Write("Введите координату x центра окружности: ");
+                                    double firstX = double.Parse(Console.ReadLine());
+                                    Console.Write("Введите координату y центра окружности: ");
+                                    double firstY = double.Parse(Console.ReadLine());
+                                    Console.Write("Введите координату x центра окружности: ");
+                                    double secondX = double.Parse(Console.ReadLine());
+                                    Console.Write("Введите координату y центра окружности: ");
+                                    double secondY = double.Parse(Console.ReadLine());
+                                    editor.Create(new Line(new Point(firstX, firstY), new Point(secondX, secondY)));
+                                }
+                                catch (Exception)
+                                {
+                                    Console.WriteLine("Некорректный ввод");
+                                }
+
+                                break;
+                            case '2':
+                                Point point;
+                                double radius;
+                                try
+                                {
+                                    GetCircleInfo(out point, out radius);
+                                    editor.Create(new Circle(point, radius));
+                                }
+                                catch (Exception)
+                                {
+                                    Console.WriteLine("Некорректный ввод");
+                                }
+
+                                break;
+                            case '3':
+                                try
+                                {
+                                    GetCircleInfo(out point, out radius);
+                                    editor.Create(new Round(point, radius));
+                                }
+                                catch (Exception)
+                                {
+                                    Console.WriteLine("Некорректный ввод");
+                                }
+
+                                break;
+                            case '4':
+                                try
+                                {
+                                    GetCircleInfo(out point, out radius);
+                                    double innerRadius;
+                                    do
+                                    {
+                                        Console.Write("Введите внутренний радиус: ");
+                                        innerRadius = double.Parse(Console.ReadLine());
+
+                                        if (innerRadius <= 0)
+                                        {
+                                            Console.WriteLine("Некорректный ввод: радиус должен быть больше нуля");
+                                        }
+                                    }
+                                    while (radius <= 0);
+
+                                    editor.Create(new Ring(point, innerRadius, radius));
+                                }
+                                catch (Exception)
+                                {
+                                    Console.WriteLine("Некорректный ввод");
+                                }
+                                
+                                break;
+                            case '5':
+                                try
+                                {
+                                    Console.Write("Введите координату x левого верхнего угла прямоугольника: ");
+                                    double firstX = double.Parse(Console.ReadLine());
+                                    Console.Write("Введите координату y левого верхнего угла прямоугольника: ");
+                                    double firstY = double.Parse(Console.ReadLine());
+                                    double width, height;
+                                    do
+                                    {
+                                        Console.Write("Введите ширину прямоугольника: ");
+                                        width = double.Parse(Console.ReadLine());
+                                        if (width <= 0)
+                                        {
+                                            Console.WriteLine("Некорректный ввод: ширина должна быть больше ноля");
+                                        }
+
+                                    }
+                                    while (width <= 0);
+
+                                    do
+                                    {
+                                        Console.Write("Введите высота прямоугольника: ");
+                                        height = double.Parse(Console.ReadLine());
+                                        if (height <= 0)
+                                        {
+                                            Console.WriteLine("Некорректный ввод: высота должна быть больше ноля");
+                                        }
+
+                                    }
+                                    while (width <= 0);
+
+                                    editor.Create(new Rectangle(new Point(firstX, firstY), width, height));
+                                }
+                                catch (Exception)
+                                {
+                                    Console.WriteLine("Некорректный ввод");
+                                }
+
+                                break;
+                            default:
+                                Console.WriteLine("Некорректный ввод!");
+                                break;
+                        }
+
+                        break;
+                    case '2':
+                        if (editor.CreatedFigures.Count == 0)
+                        {
+                            Console.WriteLine("Не создано ни одной фигуры");
+                        }
+
+                        foreach (Figure item in editor.CreatedFigures)
+                        {
+                            editor.Draw(item);
+                        }
+
+                        break;
+                    default:
+                        Console.WriteLine("Некорректный ввод!");
+                        break;
+                }
+            }
         }
     }
 }
--- Task02/Task02-07/VectorGraphicsEditor.cs
+++ Task02/Task02-07/VectorGraphicsEditor.cs
@@ -1,11 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
 using FigureLibrary;
 
 namespace Task02_07
 {
     internal class VectorGraphicsEditor
     {
-        public static void Draw(Line line)
+        private HashSet<Figure> createdFigures = new HashSet<Figure>();
+
+        public HashSet<Figure> CreatedFigures { get => this.createdFigures; }
+
+        public void Draw(Figure figure)
+        {
+            switch (figure.GetType().ToString())
+            {
+                case "FigureLibrary.Line":
+                    {
+                        this.Draw((Line)figure);
+                        break;
+                    }
+
+                case "FigureLibrary.Circle":
+                    {
+                        this.Draw((Circle)figure);
+                        break;
+                    }
+
+                case "FigureLibrary.Round":
+                    {
+                        this.Draw((Round)figure);
+                        break;
+                    }
+
+                case "FigureLibrary.Ring":
+                    {
+                        this.Draw((Ring)figure);
+                        break;
+                    }
+
+                case "FigureLibrary.Rectangle":
+                    {
+                        this.Draw((Rectangle)figure);
+                        break;
+                    }
+            }
+        }
+        
+        public void Create(Line line)
+        {
+            this.createdFigures.Add(line);
+        }
+
+        public void Create(Circle circle)
+        {
+            this.createdFigures.Add(circle);
+        }
+
+        public void Create(Round round)
+        {
+            this.createdFigures.Add(round);
+        }
+
+        public void Create(Ring ring)
+        {
+            this.createdFigures.Add(ring);
+        }
+
+        public void Create(Rectangle rect)
+        {
+            this.createdFigures.Add(rect);
+        }
+
+        private void Draw(Line line)
         {
             Console.WriteLine("Тип фигуры: Линия");
             Console.WriteLine("Параметры:");
@@ -13,7 +79,7 @@ namespace Task02_07
             Console.WriteLine($"Точка конца: ({line.End.X},{line.End.Y})");
         }
 
-        public static void Draw(Circle circle)
+        private void Draw(Circle circle)
         {
             Console.WriteLine("Тип фигуры: Окружность");
             Console.WriteLine("Параметры:");
@@ -22,7 +88,7 @@ namespace Task02_07
             Console.WriteLine($"Длина окружности: {circle.Circumference}");
         }
 
-        public static void Draw(Round round)
+        private void Draw(Round round)
         {
             Console.WriteLine("Тип фигуры: Круг");
             Console.WriteLine("Параметры:");
@@ -31,8 +97,8 @@ namespace Task02_07
             Console.WriteLine($"Длина окружности: {round.Circumference}");
             Console.WriteLine($"Площадь: {round.Area}");
         }
-        
-        public static void Draw(Ring ring)
+
+        private void Draw(Ring ring)
         {
             Console.WriteLine("Тип фигуры: Кольцо");
             Console.WriteLine("Параметры:");
@@ -43,7 +109,7 @@ namespace Task02_07
             Console.WriteLine($"Площадь: {ring.Area}");
         }
 
-        public static void Draw(Rectangle rect)
+        private void Draw(Rectangle rect)
         {
             Console.WriteLine("Тип фигуры: Прямоугольник");
             Console.WriteLine("Параметры:");
