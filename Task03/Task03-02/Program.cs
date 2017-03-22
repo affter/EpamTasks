ac12498d9b4dcd6c9da4ae69b4768f5d0bade3cb
affter <after-shock@ya.ru>
2017-03-22 08:08:02 +0400

    Added Task03-02

---
+++ Task03/Task03-02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task03_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            char[] separators = { ' ', '.' };
+            var wordsFrequency = new Dictionary<string, double>();
+
+            Console.WriteLine("Введите текст:");
+            text = Console.ReadLine().ToLower();
+            string[] words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            CalculateFrequency(wordsFrequency, words);
+
+            foreach (var word in wordsFrequency)
+            {
+                Console.WriteLine($"Частота, с которой слово {word.Key} встречается в тексте: {word.Value.ToString()}");
+            }
+        }
+
+        private static void CalculateFrequency(Dictionary<string, double> wordsFrequency, string[] words)
+        {
+            double length = words.Length;
+            for (int i = 0; i < length; i++)
+            {
+                double value = 0;
+                double increment = 1 / length;
+                if (wordsFrequency.TryGetValue(words[i], out value))
+                {
+                    wordsFrequency[words[i]] = value + increment;
+                }
+                else
+                {
+                    wordsFrequency.Add(words[i], increment);
+                }
+            }
+        }
+    }
+}
