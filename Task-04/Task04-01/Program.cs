ccf11bba25f7e1bca953a421f7871135a7da993f
affter <after-shock@ya.ru>
2017-03-26 22:18:20 +0400

    Added Task04-01

---
+++ Task-04/Task04-01/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04_01
+{
+    public class Program
+    {
+        public static void QSort<T>(T[] array, Func<T, T, bool> comparisonMethod) => QSort(array, 0, array.Length - 1, comparisonMethod);
+
+        public static void QSort<T>(T[] array, int first, int last, Func<T, T, bool> comparisonMethod)
+        {
+            T temp;
+            T x = array[first + ((last - first) / 2)];
+            int i = first;
+            int j = last;
+            while (i <= j)
+            {
+                while (comparisonMethod(array[j], x))
+                {
+                    j--;
+                }
+
+                while (comparisonMethod(x, array[i]))
+                {
+                    i++;
+                }
+
+                if (i <= j)
+                {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+
+            if (i < last)
+            {
+                QSort(array, i, last, comparisonMethod);
+            }
+
+            if (first < j)
+            {
+                QSort(array, first, j, comparisonMethod);
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
