93064ce2b33513dd4040831168d90f0e2e6ea956
affter <after-shock@ya.ru>
2017-03-29 11:45:42 +0400

    Added Task04-05

---
+++ Task04/Task04-05/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04_05
+{
+    internal static class Program
+    {
+        public static bool IsPositiveInt(this string content)
+        {
+            if (!content.Any())
+            {
+                return false;
+            }
+
+            for (int i = 0; i < content.Length; i++)
+            {
+                if (!char.IsDigit(content[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
