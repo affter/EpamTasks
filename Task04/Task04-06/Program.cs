16c6cccbf2d68662110b611079d5cfc267e1ab10
affter <after-shock@ya.ru>
2017-03-29 12:26:55 +0400

    Started Task04-06

---
+++ Task04/Task04-06/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04_06
+{
+    internal class Program
+    {
+        private static bool ComparisonMethod(int x)
+        {
+            return x > 0;
+        }
+
+        private static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+            var array = new int[200000000];
+            var rnd = new Random();
+            var arrayLength = array.Length;
+            Predicate<int> predicate1 = delegate (int x) { return x > 0; };
+            Predicate<int> predicate2 = (x) => x > 0;
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                array[i] = rnd.Next(-arrayLength, arrayLength);
+            }
+
+            stopWatch.Start();
+            array.FindAllPositive();
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
+            stopWatch.Reset();
+        }
+    }
+}
---
+++ Task04/Task04-06/TestedMethods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04_06
+{
+    internal static class TestedMethods
+    {
+        public static IEnumerable<int> FindAllPositive(this int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                {
+                    yield return array[i];
+                }
+            }
+        }
+
+        public static IEnumerable<int> FindAll(this int[] array, Predicate<int> condition)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (condition(array[i]))
+                {
+                    yield return array[i];
+                }
+            }
+        }
+
+        public static IEnumerable<int> FindAllPositiveLinq(this int[] array)
+        {
+            var a = array.Where(n => n > 0);
+
+            return a;
+        }
+    }
+}
