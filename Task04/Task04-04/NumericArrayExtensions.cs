8aca1738c32f5ba01d953303482651e061fd7553
affter <after-shock@ya.ru>
2017-03-29 11:30:08 +0400

    Added Task04-04

---
+++ Task04/Task04-04/NumericArrayExtensions.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04_04
+{
+    internal static class NumericArrayExtensions
+    {
+        public static long Sum(this int[] array)
+        {
+            long sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return sum;
+        }
+
+        public static long Sum(this long[] array)
+        {
+            long sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return sum;
+        }
+
+        public static long Sum(this short[] array)
+        {
+            long sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return sum;
+        }
+
+        public static long Sum(this sbyte[] array)
+        {
+            long sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return sum;
+        }
+
+        public static long Sum(this byte[] array)
+        {
+            long sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return sum;
+        }
+
+        public static long Sum(this char[] array)
+        {
+            long sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return sum;
+        }
+
+        public static long Sum(this ushort[] array)
+        {
+            long sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return sum;
+        }
+
+        public static long Sum(this uint[] array)
+        {
+            long sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return sum;
+        }
+
+        public static ulong Sum(this ulong[] array)
+        {
+            ulong sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return sum;
+        }
+
+        public static float Sum(this float[] array)
+        {
+            float sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return sum;
+        }
+
+        public static double Sum(this double[] array)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return sum;
+        }
+
+        public static decimal Sum(this decimal[] array)
+        {
+            decimal sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return sum;
+        }
+    }
+}
