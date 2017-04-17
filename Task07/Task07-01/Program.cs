c3010348cceff0497e41ff5b26c0440c2be0f36a
affter <after-shock@ya.ru>
2017-04-17 09:20:20 +0400

    Added Task07-02 and code refactor

--- Task07/Task07-01/Program.cs
+++ Task07/Task07-01/Program.cs
@@ -7,20 +7,20 @@ using System.Threading.Tasks;
 
 namespace Task07_01
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Введите строку: ");
+            Console.Write("Введите текст, содержащий дату в формате dd-mm-yyyy: ");
             string input = Console.ReadLine();
             string pattern = "(?:(?:0[1-9]|[1-2][0-9])-(?:0[1-9]|1[0-2])-|30-(?:0[469]|11)-|31-(?:0[13578]|1[02])-)\\d{4}";
             if (Regex.IsMatch(input, pattern))
             {
-                Console.WriteLine("В строке есть дата в формате dd-mm-yyyy");
+                Console.WriteLine($"В тексте \"{input}\" содержится дата");
             }
             else
             {
-                Console.WriteLine("В строке нет даты в формате dd-mm-yyyy");
+                Console.WriteLine($"В тексте \"{input}\" не содержится дата");
             }
         }
     }
---
+++ Task07/Task07-02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task07_02
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Введите текст: ");
+            string input = Console.ReadLine();
+            Regex regex = new Regex("<(.+?)>");
+            string output = regex.Replace(input, "_");
+            Console.WriteLine($"Результат замены: {output}");
+        }
+    }
+}
