7e357bb9dc915029469bc6bd81a6d6b01157469f
affter <after-shock@ya.ru>
2017-05-03 16:39:08 +0400

    Few code fixes

--- Task09/Task09-04/choise.js
+++ Task09/Task09-04/choise.js
@@ -1,12 +1,16 @@
 /*jslint plusplus: true */
 
     var closeButton = document.querySelector(".close-window"),
-        restartButton = document.querySelector(".restart");
+        restartButton = document.querySelector(".restart"),
+        thisWindow;
 
 closeButton.onclick = function () {
-    var thisWindow = window.open(location,'_self');
+    thisWindow = window.open('','_self');
+    console.log(opener);
+    thisWindow.close();
 }
 
 restartButton.onclick = function () {
-    window.history.go(-3);
+    window.location.assign("index.html");
 }
+
--- Task09/Task09-04/view.js
+++ Task09/Task09-04/view.js
@@ -8,13 +8,8 @@
         nextPage = document.querySelector(".next-page"),
         interval,
         timeout;
-    interval = setInterval(function () {
-        timer.innerHTML--;
-    }, 1000)
-    timeout = setTimeout(function () {
-        window.location.assign(nextPage.innerHTML)
-    }, timer.innerHTML*1000)
 
+startTimer();
 
 stopButton.onclick = function() {
     resumeButton.disabled = false;
@@ -26,13 +21,16 @@ stopButton.onclick = function() {
 resumeButton.onclick = function () {
     resumeButton.disabled = true;
     stopButton.disabled = false;
+    startTimer();
+}
+
+function startTimer () {
     interval = setInterval(function () {
         timer.innerHTML--;
-    }, 1000)
+    }, 1000);
     timeout = setTimeout(function () {
-        window.location.assign("index2.html")
-    }, timer.innerHTML*1000)
-
+        window.window.open(nextPage.innerHTML, "_self");
+    }, timer.innerHTML*1000);
 }
 
 backButton.onclick = function () {
